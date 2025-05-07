29a617568ce4332abff015bae8d152afd68c9600
GameNotCreator <78528763+GameNotCreator@users.noreply.github.com>
2025-05-08 01:30:45 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
+function analogToTDS (analogValue: number) {
+    return Math.round(analogValue / 10)
+}
+function SendData () {
+    // Affiche la tension pendant 1 seconde
+    basic.pause(2000)
+    // Envoi de la valeur du pH à ThingSpeak en temps réel sur Field 1
+    // Envoie la valeur du pH dans Field 1
+    DFRobotWiFiIoTI2C.ThingSpeakSend("" + roundedPH, "" + tdsValue, "" + Tubidity)
+    // Affiche la tension pendant 1 seconde
+    basic.pause(2000)
+}
+function Pompes () {
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    // Affiche la tension pendant 1 seconde
+    basic.pause(2000)
+    getData()
+    // Affiche la tension pendant 1 seconde
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    // Affiche la tension pendant 1 seconde
+    basic.pause(5000)
+}
 function convertToPH (a: number) {
     // Tensions de calibration pour les solutions tampons de pH 4 et pH 7
     // Tension pour pH 4
@@ -12,23 +37,13 @@ function convertToPH (a: number) {
     pH = (a - tension_min) / (tension_max - tension_min) * (pH_max - pH_min) + pH_min + 2
     return pH
 }
-let roundedPH = 0
-let roundedTension = 0
-let pHValue = 0
-let tension = 0
-let rawValue = 0
-let pH = 0
-let pH_max = 0
-let pH_min = 0
-let tension_max = 0
-let tension_min = 0
-DFRobotWiFiIoTI2C.WIFISetup("jery", "00770077")
-DFRobotWiFiIoTI2C.ThingSpeakConfigure("6U6SANA0WHOXUEDC")
-basic.forever(function () {
+function getData () {
+    rawTDS = pins.analogReadPin(AnalogReadWritePin.P10)
+    tdsValue = analogToTDS(rawTDS)
     // Lire la valeur analogique
-    rawValue = pins.analogReadPin(AnalogPin.P0)
+    rawPH = pins.analogReadPin(AnalogReadWritePin.P0)
     // Convertir en tension (0 à 3.3V)
-    tension = rawValue * 3.3 / 1024
+    tension = rawPH * 3.3 / 1024
     // Calculer la valeur de pH
     pHValue = convertToPH(tension)
     // Arrondir la tension à 2 décimales
@@ -37,18 +52,24 @@ basic.forever(function () {
     // Arrondir la valeur du pH à 2 décimales
     // Arrondi à 2 décimales
     roundedPH = Math.round(pHValue * 100) / 100
-    // Afficher la tension mesurée et la valeur du pH sur le micro:bit
-    // Afficher la tension mesurée
-    basic.showNumber(roundedTension)
-    // Affiche la tension pendant 1 seconde
-    basic.pause(1000)
-    // Effacer l'écran avant d'afficher le pH
-    basic.clearScreen()
-    // Afficher le pH calculé
-    basic.showNumber(roundedPH)
-    // Envoi de la valeur du pH à ThingSpeak en temps réel sur Field 1
-    // Envoie la valeur du pH dans Field 1
-    DFRobotWiFiIoTI2C.ThingSpeakSend("" + roundedPH)
-    // Envoi chaque 2 secondes (ajuste cette valeur si nécessaire)
-    basic.pause(2000)
+    Tubidity = pins.digitalReadPin(DigitalPin.P6)
+}
+let roundedTension = 0
+let pHValue = 0
+let tension = 0
+let rawPH = 0
+let rawTDS = 0
+let pH = 0
+let pH_max = 0
+let pH_min = 0
+let tension_max = 0
+let tension_min = 0
+let Tubidity = 0
+let tdsValue = 0
+let roundedPH = 0
+DFRobotWiFiIoTI2C.WIFISetup("jery", "00770077")
+DFRobotWiFiIoTI2C.ThingSpeakConfigure("6U6SANA0WHOXUEDC")
+basic.forever(function () {
+    getData()
+    SendData()
 })
